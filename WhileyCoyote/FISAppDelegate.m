7e15f4d63bea57f264d36af1c5ac75a48b10729b
magfurulabeer <magfurulabeer@gmail.com>
2015-12-29 01:03:20 -0500

    Complete program

--- WhileyCoyote/FISAppDelegate.m
+++ WhileyCoyote/FISAppDelegate.m
@@ -11,6 +11,19 @@
     
      */
     
+    NSUInteger steps = 0;
+    NSUInteger anvil = arc4random_uniform(25) + 26;
+    do {
+        if (steps > 0 && steps%10 == 0) {
+            NSLog(@"YOU'RE CUCKOO!");
+        }
+        
+        if (steps == anvil) {
+            NSLog(@"SMASH!");
+        }
+        
+        NSLog(@"%lu. Meep! Meep!", ++steps);
+    } while (steps <= 50);
     // do not alter
     return YES;  //
     ///////////////
