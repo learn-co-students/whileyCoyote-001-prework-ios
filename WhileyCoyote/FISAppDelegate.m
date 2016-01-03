5736385cd1efdb7cccc707f21f677fab1dd2c62c
shibamo <qinchao@hotmail.com>
2016-01-03 12:39:53 -0500

    try while

--- WhileyCoyote/FISAppDelegate.m
+++ WhileyCoyote/FISAppDelegate.m
@@ -5,11 +5,9 @@
 @implementation FISAppDelegate
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     
-    /**
-     
-     * Write your loop here!
-    
-     */
+    do{
+        NSLog(@"Meep! Meep!");
+    }while (YES);
     
     // do not alter
     return YES;  //
