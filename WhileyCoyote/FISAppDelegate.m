b73b1b54d9f74d8d45b8135d461ffc15517420ab
jeremyfeld <jeremyfeld@gmail.com>
2016-01-05 21:51:59 -0500

    add do-while loop

--- WhileyCoyote/FISAppDelegate.m
+++ WhileyCoyote/FISAppDelegate.m
@@ -10,6 +10,19 @@
      * Write your loop here!
     
      */
+    NSUInteger steps = 0;
+    NSUInteger anvil = arc4random_uniform(10) + 41;
+    do {
+        if (steps > 0 && steps % 10 ==0) {
+            NSLog(@"YOU'RE CUCKOO!");
+        }
+        if (steps == anvil) {
+            NSLog(@"SMASH!");
+        }
+        NSLog(@"Meep! Meep!");
+        steps++;
+        NSLog(@"%lu", steps);
+    } while (steps <= anvil);
     
     // do not alter
     return YES;  //
