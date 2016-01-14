1e287d21b2d903e6aca0255f0e79412dd22fe583
bclous <brian.clouser@gmail.com>
2016-01-14 11:37:01 -0500

    finished lesson dropped anvil on coyote

--- WhileyCoyote/FISAppDelegate.m
+++ WhileyCoyote/FISAppDelegate.m
@@ -11,6 +11,31 @@
     
      */
     
+    NSUInteger steps = 0;
+    
+    NSUInteger anvil = arc4random_uniform(25) + 26;
+    
+    do
+    {
+        BOOL stepsTen = steps%10 == 0;
+        
+        if(stepsTen)
+        {
+            NSLog(@"YOU'RE CUCKOO!");
+        }
+        
+        if(steps == anvil)
+        {
+            NSLog(@"SMASH!");
+        }
+        
+        NSLog(@"%lu.Meep Meep!", steps);
+        steps++;
+    }
+    while (steps <=anvil);
+   
+   
+    
     // do not alter
     return YES;  //
     ///////////////
