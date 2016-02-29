709158ca2dcd1f786d92fb751e0b7be0f81d38e3
uArendt <jarendt66@me.com>
2016-02-29 15:52:23 -0500

    wiley cyote is no match for this random loop

--- WhileyCoyote/FISAppDelegate.m
+++ WhileyCoyote/FISAppDelegate.m
@@ -10,26 +10,23 @@
      * Write your loop here!
     
      */
+
     NSUInteger steps = 0;
-    NSUInteger anvil = arc4random_uniform(25) + 26;
+    NSUInteger anvil = arc4random_uniform(25)+26;
+
     
     do {
-        if (steps % 10 == 0 && steps > 0) {
+        if (steps % 10 ==0 && steps > 0) {
             NSLog(@"YOU'RE CUCKOO!");
         }
         if (steps == anvil) {
-            NSLog(@"SQUASH!!!");
+            NSLog(@"SQUISH!");
+            
+            //steps =0; this command will restart the loop at 0 and creat an infinite loop
         }
-        NSLog(@"MEEP MEEP!");
-        steps++;
-    } while (steps <= anvil); {
-        
-    }
-    
-    
-    
-    
-    
+        NSLog(@"Meep Meep! %lu", (unsigned long)steps);
+        steps ++;
+    } while (steps <= anvil);
     
     // do not alter
     return YES;  //
