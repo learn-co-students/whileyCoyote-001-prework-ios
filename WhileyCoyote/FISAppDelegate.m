5099641870717a2869494481b1f7bbb11b9c73df
Dougly <dgalante11@gmail.com>
2016-03-02 18:30:39 -0500

    example do while loop

--- WhileyCoyote/FISAppDelegate.m
+++ WhileyCoyote/FISAppDelegate.m
@@ -5,11 +5,22 @@
 @implementation FISAppDelegate
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     
-    /**
-     
-     * Write your loop here!
+    NSUInteger steps = 0;
+    // will chose an intiger between 10000 and 10025.
+    NSUInteger anvil = arc4random_uniform(25)+10000;
     
-     */
+    do {
+        BOOL wiley = steps % 10 == 0;
+        if (wiley && steps > 0) {
+            NSLog(@"YOU'RE CUCKOO!");
+        }
+        if (steps == anvil) {
+            NSLog(@"SMASH!");
+        }
+        NSLog(@"%lu: Meep! Meep!", steps + 1);
+        steps++;
+        
+    }while (steps <= anvil);
     
     // do not alter
     return YES;  //
