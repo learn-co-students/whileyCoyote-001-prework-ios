f6a65b7a11b89746fa0785569b5c0492caaac58b
dochonglo <ismaelbarry777@hotmail.com>
2016-04-14 21:50:58 -0400

    Lab 15: Code Along (Whiley Coyote) is complete.

--- WhileyCoyote/FISAppDelegate.m
+++ WhileyCoyote/FISAppDelegate.m
@@ -11,6 +11,24 @@
     
      */
     
+    NSUInteger steps = 0;
+    NSUInteger anvil = arc4random_uniform(10) + 11; // Generates a random integer from 11 to 20.
+    
+    do  {
+        if (((steps%10) == 0) && (steps > 0)) {
+            NSLog(@"YOU'RE CUCKOO!");
+        }
+        
+        if (steps == anvil) {
+            NSLog(@"SMASH!");
+        }
+        
+        NSLog(@"MEEP! MEEP!: %li", steps);
+        steps++;
+    } while (steps <= anvil); {
+        
+    }
+    
     // do not alter
     return YES;  //
     ///////////////
