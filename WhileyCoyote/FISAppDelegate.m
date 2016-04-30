a85122e61c72a109a06da12facb5de4cfa10d388
jellybeanjohnny <mamerige@icloud.com>
2016-04-30 10:24:22 -0400

    Added all code for this lesson

--- WhileyCoyote/FISAppDelegate.m
+++ WhileyCoyote/FISAppDelegate.m
@@ -10,6 +10,19 @@
      * Write your loop here!
     
      */
+    NSUInteger anvil = arc4random_uniform(25) + 26;
+    NSUInteger steps = 0;
+    
+    do {
+        if (steps % 10 == 0) {
+            NSLog(@"YOU'RE CUCKOO!");
+        }
+        if (steps == anvil) {
+            NSLog(@"SMASH!");
+        }
+        NSLog(@"Meep! Meep!");
+        steps++;
+    } while (steps <= anvil);
     
     // do not alter
     return YES;  //
