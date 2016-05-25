6ee48200c7ed51298d003cbba2aa24ef9e702351
Elli Scharlin <escharlin@wesleyan.edu>
2016-05-25 18:46:56 -0400

    Runs randomly
    Completed the advanced challenge as well as the regular one.

--- WhileyCoyote/FISAppDelegate.m
+++ WhileyCoyote/FISAppDelegate.m
@@ -4,12 +4,23 @@
 
 @implementation FISAppDelegate
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-    
-    /**
-     
-     * Write your loop here!
-    
-     */
+    /*
+     Define a new NSUInteger anvil variable before the loop and set it equal to arc4random_uniform(25) + 26. This generates a random integer from 26 to 50. Replace the two occurrences of 50 in the conditionals with the anvil variable.
+     Hit run a few different times. Notice that the Road Runner drops the anvil after a different number of steps each time.
+     Play around with the range of the anvil variable by changing the integers that affect the usage of arc4random_uniform().     Meep! Meep!
+    */
+    NSUInteger steps = 0;
+    NSUInteger anvil = arc4random_uniform(25)+26;
+    do {
+        if (steps%10 == 0 && steps != 0) {
+            NSLog(@"YOU'RE CUCKOO!");
+        }
+        if (steps == anvil) {
+            NSLog(@"SMASH BANG BOOM");
+        }
+        NSLog(@"Meep! Meep!");
+        steps++;
+    } while (steps <=anvil);
     
     // do not alter
     return YES;  //
