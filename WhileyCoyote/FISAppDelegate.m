0914110f48984485ec6bc2154634baf230febaeb
William Robinson <wsrobins@uvm.edu>
2016-06-28 13:11:00 -0400

    Whiley Coyote loop practice // lab complete.

--- WhileyCoyote/FISAppDelegate.m
+++ WhileyCoyote/FISAppDelegate.m
@@ -5,11 +5,25 @@
 @implementation FISAppDelegate
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     
-    /**
-     
-     * Write your loop here!
+    NSUInteger steps = 0;
+    NSUInteger anvil = arc4random_uniform(23) + 14;
     
-     */
+    do {
+        
+        if (steps % 10 == 0 && steps > 0) {
+            
+            NSLog(@"YOU'RE CUCKOO!");
+        }
+        if (steps == anvil) {
+            
+            NSLog(@"SMASH!");
+        }
+        
+        NSLog(@"Meep! Meep!");
+        
+        steps++;
+        
+    } while (steps <= anvil);
     
     // do not alter
     return YES;  //
